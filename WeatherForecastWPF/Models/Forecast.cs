b4dceca2da82cdcf378a7f83fe6705b718cbf64e
Chelovek <nastya28042020@gmail.com>
2024-12-25 12:22:57 +0500

    Сохранение

---
+++ WeatherForecastWPF/Models/Forecast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecastWPF.Models
+{
+    public class Forecast
+    {
+        public List<DayPart> Today = new List<DayPart>();
+        public List<DayPart> Week = 
+    }
+}
