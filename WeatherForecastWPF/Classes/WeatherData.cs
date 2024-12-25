895e1c7c27f23d00d8fc2393de536f37a98ab742
ElovikovStepan <selovikov12345@gmail.com>
2024-12-26 01:54:32 +0500

    Разработан API. Делали вместе (Еловиков и Глебов)

--- WeatherForecastWPF/Classes/WeatherData.cs
+++ WeatherForecastWPF/Classes/WeatherData.cs
@@ -1,29 +1,26 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows.Documents;
 
 namespace WeatherForecastWPF.Classes
 {
 	public class WeatherData
 	{
-		public static List<WeatherJsonParser> Weather = new List<WeatherJsonParser>();
-
-		public string ApiKey = "GaEDdqRrmwA3VAqEEpNoHKGWpJKEtvRv";
-		public string LanguageKey = "ru";
-		public int PermLocationKey = 294922;
-		public int MoscowLocationKey = 294021;
-		public async Task<string> GetWeatherData(int LocationKey)
+		private string ApiKey = "GaEDdqRrmwA3VAqEEpNoHKGWpJKEtvRv";
+		private string LanguageKey = "ru";
+		public static string OneDayKey = "1day";
+		public static string FiveDayKey = "5day";
+		public static int PermLocationKey = 294922;
+		public static int MoscowLocationKey = 294021;
+		public async Task<string> GetWeatherData(string DayKey, int LocationKey)
 		{
 			// URL строка запроса
-			string requestUrl = $"http://dataservice.accuweather.com/forecasts/v1/daily/1day/294922" +
-					$"?apikey={ApiKey}" +
-					$"&language={LanguageKey}" +
-					$"&metric=true" +
-					$"&details=true";
+			string requestUrl = $"http://dataservice.accuweather.com/forecasts/v1/daily" +
+				$"/{DayKey}" +
+				$"/{LocationKey}" +
+				$"?apikey={ApiKey}" +
+				$"&language={LanguageKey}" +
+				$"&metric=true" +
+				$"&details=true";
 			// Ответ API
 			var responseApi = await SendWeatherRequest(requestUrl, LocationKey);
 
@@ -48,42 +45,6 @@ namespace WeatherForecastWPF.Classes
 			// Возвращение данных
 			return await requestData;
 		}
-        public static void ParseWeatherJson(string json)
-        {
-            try
-            {
-				// Отчистка списка с прогнозом погоды
-				Weather.Clear();
-                // Парсинг JSON
-                var jsonObject = JObject.Parse(json);
-                var dailyForecast = jsonObject["DailyForecasts"]?[0];
 
-                if (dailyForecast != null)
-                {
-                    // Извлечение данных для дня
-                    Console.WriteLine("Данные для дня:");
-                    Weather.Add(new WeatherJsonParser(dailyForecast["Day"], "Пермь", "День"));
-
-                    // Извлечение данных для ночи
-                    Console.WriteLine("\nДанные для ночи:");
-                    Weather.Add(new WeatherJsonParser(dailyForecast["Night"], "Пермь", "День"));
-
-                    Debug.Listeners.Add(new TextWriterTraceListener("log.txt"));
-                    foreach (WeatherJsonParser i in Weather)
-                    {
-                        Debug.WriteLine(i.Cloudy);
-                        Debug.Flush();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Прогноз не найден в JSON-файле.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка при обработке JSON: {ex.Message}");
-            }
-        }
-    }
+	}
 }
--- WeatherForecastWPF/MainWindow.xaml.cs
+++ WeatherForecastWPF/MainWindow.xaml.cs
@@ -1,33 +1,46 @@
-﻿using System.Windows;
-using WeatherForecastWPF.Classes;
+﻿using System;
 using System.IO;
-using System;
+using System.Windows;
+using System.Windows.Documents;
+using WeatherForecastWPF.Classes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace WeatherForecastWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
 	{
+		public static int Period = 5;
+		public static int LocationKey = -1;
+		List<List<WeatherJsonParser>> AllPeriodWeatherJsonParser = new List<List<WeatherJsonParser>>();
 		public MainWindow()
 		{
 			InitializeComponent();
 			WeatherData weatherDataClient = new WeatherData();
-			// Если содержаться данные, то обращаться к API не нужно (Ограничение в 50 запросов в день)
-			if (File.ReadAllText("../../Files/SavedApiJson.txt") == String.Empty)
+			// Если содержаться все данные, то обращаться к API не нужно (Ограничение на 50 запросов в день)
+			if (File.ReadAllLines("../../Files/SavedApiJson.txt").Length == 0)
 			{
-				var rawApiData = weatherDataClient.GetWeatherData(294922);
+				Task<string> rawApiData = null;
+
+				switch (Period)
+				{
+					case 1: rawApiData = weatherDataClient.GetWeatherData(WeatherData.OneDayKey, LocationKey); break;
+					case 5: rawApiData = weatherDataClient.GetWeatherData(WeatherData.FiveDayKey, LocationKey); break;
+				}
+				StreamWriter streamWriter = new StreamWriter("../../Files/SavedApiJson.txt");
+				// Чтение данных
 				var normalizeApiData = rawApiData.Result.ToString();
-                // Сохранение в файл
-				StreamWriter streamWriter = new StreamWriter(".. / .. / Files / SavedApiJson.txt");
+				// Сохранение в файл
 				streamWriter.WriteLine(normalizeApiData);
 				streamWriter.Close();
 			}
 			// Создание эксземпляра класса с фактическими параметрами
-			string jsonNormalizedApiData= File.ReadAllText("../../Files/SavedApiJson.txt");
-			WeatherData.ParseWeatherJson(jsonNormalizedApiData);
-        }
+			string jsonNormalizedApiData = File.ReadAllText("../../Files/SavedApiJson.txt");
+			WeatherJsonParser.ParseWeatherJson(jsonNormalizedApiData, ref AllPeriodWeatherJsonParser);
+		}
 	}
 }
