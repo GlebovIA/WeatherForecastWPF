a3f43ee4d4948eea3bb0e9500b4ce7b896ac7c0e
student-a502 <student-a502@PC-A502-19>
2024-12-25 13:10:22 +0500

    Создание класса с постоянными значениями. Еловиков

---
+++ WeatherForecastWPF/Models/ConstData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecastWPF.Models
+{
+    public static class ConstData
+    {
+        public static string ApiKey = "tDZyrBCCajFnMNTpoOSPtaVlAAEDL8Zb";
+        public static int PermLocationKey = 294922;
+        public static int MoscowLocationKey = 294021;
+        public static string LanguageKey = "ru";
+    }
+}
